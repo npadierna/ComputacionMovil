8eb46da5eb9c0689dfe473cfa13a67ea8e333ac7
Neiber Padierna Pérez <npadierna@gmail.com>
2014-06-27 07:44:21 -0500

    SQL Creator Script
    Updating the SQL Creator Script adding mock data for Data base tables.

--- FinalProject/WebApplication/ERModel/Scripts/Script_Loader.sql
+++ FinalProject/WebApplication/ERModel/Scripts/Script_Loader.sql
@@ -3,17 +3,21 @@
 -- -----------------------------------------------------
 -- INSERT INTO `AhorcaTooth`.`LANGUAGES` (`iso_code`, `tongue`, `description`)
 --    VALUES (NULL, NULL, NULL);
+
+BEGIN TRANSACTION;
 INSERT INTO `AhorcaTooth`.`LANGUAGES` (`iso_code`, `tongue`, `description`)
     VALUES ('en', 'English', '[EN] Language family: Indo-European. ISO 639-2/T: eng. ISO 639-2/B: eng. ISO 639-3: eng. ISO 639-6: engs.');
 INSERT INTO `AhorcaTooth`.`LANGUAGES` (`iso_code`, `tongue`, `description`)
     VALUES ('es', 'Spanish', '[ES] Language family: Indo-European. (Castillian). ISO 639-2/T: spa. ISO 639-2/B: spa. ISO 639-3: spa.');
-
+COMMIT;
 
 -- -----------------------------------------------------
 -- Table `AhorcaTooth`.`CATEGORY`
 -- -----------------------------------------------------
 -- INSERT INTO `AhorcaTooth`.`CATEGORY` (`category_name`, `languages_iso_code`, `image_name`, `description`)
 --    VALUES (NULL, NULL, NULL, NULL);
+
+BEGIN TRANSACTION;
 INSERT INTO `AhorcaTooth`.`CATEGORY` (`category_name`, `languages_iso_code`, `image_name`, `description`)
     VALUES ('COMIDAS', 'es', 'ic_foods_category.png', 'Comidas famosas en el mundo.');
 INSERT INTO `AhorcaTooth`.`CATEGORY` (`category_name`, `languages_iso_code`, `image_name`, `description`)
@@ -50,10 +54,11 @@ INSERT INTO `AhorcaTooth`.`CATEGORY` (`category_name`, `languages_iso_code`, `im
     VALUES ('OCIO', 'es', 'ic_leisure_category.png', 'Hobbies, actividades o acciones del día a día.');
 INSERT INTO `AhorcaTooth`.`CATEGORY` (`category_name`, `languages_iso_code`, `image_name`, `description`)
     VALUES ('LEISURE', 'en', 'ic_leisure_category.png', 'Hobbies, activities or actions in the everyday.');
+COMMIT;
 
---| EDUCACION  | ic_education_category.png  |
---| HOGAR      | ic_home_category.png       |
---| ROPA       | ic_clothes_category.png    |
+-- | EDUCACION  | ic_education_category.png  |
+-- | HOGAR      | ic_home_category.png       |
+-- | ROPA       | ic_clothes_category.png    |
 
 
 -- -----------------------------------------------------
@@ -61,5 +66,211 @@ INSERT INTO `AhorcaTooth`.`CATEGORY` (`category_name`, `languages_iso_code`, `im
 -- -----------------------------------------------------
 -- INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
 --    VALUES (NULL, NULL, NULL, NULL);
+
+-- COMIDAS / FOODS
+BEGIN TRANSACTION;
 INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
-    VALUES ('', NULL, NULL, NULL);
+    VALUES ('AGRIO', NULL, 'COMIDAS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('SOUR', NULL, 'FOODS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CARTA', NULL, 'COMIDAS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CARTE', NULL, 'FOODS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('HIERBA', NULL, 'COMIDAS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('GRASS', NULL, 'FOODS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('PROBAR', NULL, 'COMIDAS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TASTE', NULL, 'FOODS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('SABROSO', NULL, 'COMIDAS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TASTY', NULL, 'FOODS', 'en');
+COMMIT;
+
+-- DEPORTES / SPORTS
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('AUTOMOVILISMO', NULL, 'DEPORTES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('MOTORSPORT', NULL, 'SPORTS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('BASQUET', NULL, 'DEPORTES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('BASKETBALL', NULL, 'SPORTS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('ESGRIMA', NULL, 'DEPORTES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('SWORDPLAY', NULL, 'SPORTS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('KARATE', NULL, 'DEPORTES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('KARATE', NULL, 'SPORTS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TAEKWONDO', NULL, 'DEPORTES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TAEKWONDO', NULL, 'SPORTS', 'en');
+COMMIT;
+
+-- GEOGRAFIA / GEOGRAPHY
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('AFRICA', NULL, 'GEOGRAFIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('AFRICA', NULL, 'GEOGRAPHY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('AMERICA', NULL, 'GEOGRAFIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('AMERICA', NULL, 'GEOGRAPHY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CONTINENTE', NULL, 'GEOGRAFIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CONTINENT', NULL, 'GEOGRAPHY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('EUROPA', NULL, 'GEOGRAFIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('EUROPE', NULL, 'GEOGRAPHY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('OCEANO', NULL, 'GEOGRAFIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('OCEAN', NULL, 'GEOGRAPHY', 'en');
+COMMIT;
+
+-- MUSICA / MUSIC
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('BALADA', NULL, 'MUSICA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('BALLAD', NULL, 'MUSIC', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CLASICA', NULL, 'MUSICA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CLASSIC', NULL, 'MUSIC', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('RAGGAE', NULL, 'MUSICA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('RAGGAE', NULL, 'MUSIC', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('METAL', NULL, 'MUSICA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('METAL', NULL, 'MUSIC', 'en');
+COMMIT;
+
+-- RELIGIONES / RELIGIONS
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('BUDISMO', NULL, 'RELIGIONES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('BUDDHISM', NULL, 'RELIGIONS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CRISTIANISMO', NULL, 'RELIGIONES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CHRISTIANITY', NULL, 'RELIGIONS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('HINDUISMO', NULL, 'RELIGIONES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('HINDUISM', NULL, 'RELIGIONS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('LUTERANISMO', NULL, 'RELIGIONES', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('LUTHERANISM', NULL, 'RELIGIONS', 'en');
+COMMIT;
+
+-- TECNOLOGIA / TECHNOLOGY
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CELULAR', NULL, 'TECNOLOGIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CELLPHONE', NULL, 'TECHNOLOGY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('IMPRESORA', NULL, 'TECNOLOGIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('PRINTER', NULL, 'TECHNOLOGY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('INTERNET', NULL, 'TECNOLOGIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('INTERNET', NULL, 'TECHNOLOGY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TECLADO', NULL, 'TECNOLOGIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('KEYBOARD', NULL, 'TECHNOLOGY', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('VIDEOJUEGO', NULL, 'TECNOLOGIA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('VIDEOGAME', NULL, 'TECHNOLOGY', 'en');
+COMMIT;
+
+-- VERBOS / VERBS
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('JUGAR', NULL, 'VERBOS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('PLAY', NULL, 'VERBS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('LLAMAR', NULL, 'VERBOS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CALL', NULL, 'VERBS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('PEDIR', NULL, 'VERBOS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('DEMAND', NULL, 'VERBS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TOCAR', NULL, 'VERBOS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TOUCH', NULL, 'VERBS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TRABAJAR', NULL, 'VERBOS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('WORK', NULL, 'VERBS', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('NADAR', NULL, 'VERBOS', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('SWIM', NULL, 'VERBS', 'en');
+COMMIT;
+
+-- NATURALEZA / NATURE
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('ALUD', NULL, 'NATURALEZA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('AVALANCHE', NULL, 'NATURE', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('CONEJO', NULL, 'NATURALEZA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('RABBIT', NULL, 'NATURE', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('LLUVIOSO', NULL, 'NATURALEZA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('RAINY', NULL, 'NATURE', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TRONCO', NULL, 'NATURALEZA', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TRUNK', NULL, 'NATURE', 'en');
+COMMIT;
+
+-- OCIO / LEISURE
+BEGIN TRANSACTION;
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('APLAUSOS', NULL, 'OCIO', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('APPLAUSE', NULL, 'LEISURE', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('ARTE', NULL, 'OCIO', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('ART', NULL, 'LEISURE', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('HINCHA', NULL, 'OCIO', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('FAN', NULL, 'LEISURE', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('SENTIMENTAL', NULL, 'OCIO', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('EMOTIONAL', NULL, 'LEISURE', 'en');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TIEMPO', NULL, 'OCIO', 'es');
+INSERT INTO `AhorcaTooth`.`HANGMAN_WORD` (`word_name`, `description`, `category_name`, `languages_iso_code`)
+    VALUES ('TIME', NULL, 'LEISURE', 'en');
+COMMIT;
